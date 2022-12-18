3365b354e43c51246bb6403181fb210c7e0a79e7
Alex Ma <explodingaming10@gmail.com>
2022-12-18 00:04:59 -0800

    chat features

--- app/src/routes/(app)/test/chat/__layout.svelte
+++ app/src/routes/(app)/test/chat/__layout.svelte
@@ -2,4 +2,41 @@
 	import '../app.css';
 </script>
 
+<script lang='ts'>
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { Spinner } from '$components/core';
+	import ToastContainer from '$components/core/Toast/ToastContainer.svelte';
+	import 'bootstrap-icons/font/bootstrap-icons.css';
+
+	let ready = false;
+
+	const darkModeToggle = () => {
+		// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+		if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	};
+
+	onMount(() => {
+		darkModeToggle();
+		ready = true;
+	});
+</script>
+<div id='wrapper' class='dark:bg-dark-2 flex flex-col'>
+	<div class='app-height app-wrapper-layout'>
+		{#if ready}
+			<slot />
+		{:else}
+			<div class='h-full grid place-content-center w-full'>
+				<Spinner className='h-10 w-10' />
+			</div>
+		{/if}
+	</div>
+	<ToastContainer />
+</div>
+
+
 <main><slot /></main>
